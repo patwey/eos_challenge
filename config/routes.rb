de5e7145ccaabab4c2bc4a37e735a2e3d3396815
Pat Wey <patrickwey@gmail.com>
2016-04-18 12:46:19 -0700

    Add route to Results api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+  namespace :api do
+    namespace :v1 do
+      get '/results/:str', to: 'results#create', defaults: { format: :json }
+    end
+  end
 end
